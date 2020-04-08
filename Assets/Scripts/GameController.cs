688dfb7cd52158951390831f136c700fb31accfb
Nabson Paiva <nabsonp@gmail.com>
2020-04-07 21:43:06 -0400

    Cenário criado.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
 
-    public Transform playerTransform;
+    public Transform playerTransform, limiteCamEsq, limiteCamDir, limiteCamCima, limiteCamBaixo;
+    public float speedCam;
     private Camera cam;
 
     // Start is called before the first frame update
@@ -19,9 +20,27 @@ public class GameController : MonoBehaviour
     {
 
     }
+    void camController() {
+        float posicaoCamX = playerTransform.position.x;
+        float posicaoCamY = playerTransform.position.y;
 
+        if (cam.transform.position.x < limiteCamEsq.position.x && playerTransform.position.x < limiteCamEsq.position.x) {
+            posicaoCamX = limiteCamEsq.position.x;
+        } else if (cam.transform.position.x > limiteCamDir.position.x && playerTransform.position.x > limiteCamDir.position.x) {
+            posicaoCamX = limiteCamDir.position.x;
+        }
+
+        if (cam.transform.position.y < limiteCamBaixo.position.y && playerTransform.position.y < limiteCamBaixo.position.y) {
+            posicaoCamY = limiteCamBaixo.position.y;
+        } else if (cam.transform.position.y > limiteCamCima.position.y && playerTransform.position.y > limiteCamCima.position.y) {
+            posicaoCamY = limiteCamCima.position.y;
+        }
+
+        Vector3 posCam = new Vector3(posicaoCamX, posicaoCamY, cam.transform.position.z);
+        cam.transform.position = Vector3.Lerp(cam.transform.position, posCam, speedCam * Time.deltaTime);
+    }
     void LateUpdate() {
-        Vector3 posCam = new Vector3(playerTransform.position.x, playerTransform.position.y, cam.transform.position.z);
-        cam.transform.position = posCam;
+        camController();
     }
 }
+
