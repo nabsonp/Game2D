dbbe0a4ad7b1e48b241875a526eb524f02fe237d
Nabson Paiva <nabsonp@gmail.com>
2020-04-08 14:38:30 -0400

    Parallax criado.

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public Transform background;
+    private Camera cam;
+    public float speed;
+    private Vector3 previewCamPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        previewCamPosition = cam.transform.position;
+    }
+
+    void LateUpdate() {
+        float parallaxX = previewCamPosition.x - cam.transform.position.x;
+        float bgTargetX = background.position.x + parallaxX;
+
+        Vector3 bgPosition = new Vector3(bgTargetX, background.position.y, background.position.z);
+        background.position = Vector3.Lerp(background.position, bgPosition, speed * Time.deltaTime);
+
+        previewCamPosition = cam.transform.position;
+    
+    }
+}
